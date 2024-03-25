e121f312f1e6328b50c14a971758effac9b7640b
torihazi <lovebarusa19981225@icloud.com>
2024-03-26 07:02:37 +0900

    modify 1 + N in index,show

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,11 +2,13 @@
 
 class ProductsController < ApplicationController
   def index
-    @products = Product.page(params[:page])
+    @products = Product.preload(image_attachment: :blob)
+    @products = @products.page(params[:page])
   end
 
   def show
-    @product = Product.find(params[:id])
-    @related_products = Product.order(created_at: :desc).limit(6)
+    @product = Product.eager_load(image_attachment: :blob).find(params[:id])
+    @related_products = Product.eager_load(image_attachment: :blob)
+    @related_products = @related_products.order(created_at: :desc).limit(6)
   end
 end
