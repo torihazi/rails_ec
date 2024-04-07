e06624b0264600ec5d7067cd6c195056f5b681ca
torihazi <lovebarusa19981225@icloud.com>
2024-04-07 11:31:31 +0900

    modify table-name in cart-function

--- db/migrate/20240406061601_rename_cart_item_to_cart_product.rb
+++ db/migrate/20240406061601_rename_cart_item_to_cart_product.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class RenameCartProductToCartProduct < ActiveRecord::Migration[7.0]
+class RenameCartItemToCartProduct < ActiveRecord::Migration[7.0]
   def change
     rename_table :cart_items, :cart_products
   end
