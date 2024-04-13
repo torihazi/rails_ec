8d87aee5121ac270a92c07e91a6b5351a2d9eece
torihazi <lovebarusa19981225@icloud.com>
2024-04-13 11:47:05 +0900

    modify column_name address_line* in Order

--- db/migrate/20240406061601_rename_cart_item_to_cart_product.rb
+++ db/migrate/20240406061601_rename_cart_item_to_cart_product.rb
@@ -1,7 +1,11 @@
 # frozen_string_literal: true
 
 class RenameCartItemToCartProduct < ActiveRecord::Migration[7.0]
-  def change
-    rename_table :cart_items, :cart_products
+  def up
+    rename_table :cart_Items, :cart_products
+  end
+
+  def down
+    rename_table :cart_products, :cart_Items
   end
 end
